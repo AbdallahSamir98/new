44a6e30238c089f2e5f02ea922c00b9e095ba742
abdallah samir <abdallahsamir502@gmail.com>
2023-07-03 14:23:22 +0300

    g:

---
+++ src/pages/Customers/Customers.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Container , Placeholder} from "react-bootstrap";
+import MainPageText from "../../component/MainPageText/MainPageText";
+import TableView from "../../component/TableView/TableView";
+import { useSelector } from "react-redux";
+import SkeltonLoader from '../../component/SkeltonLoader/SkeltonLoader'
+import ErrorGettingData from "../../component/ErrorGetingData/ErrorGettingData";
+const Customers = () => {
+
+  const allCustomers = useSelector((state) => state.customer.allCustomers);
+  const formattedCustomers = allCustomers.map(({first_name , ...e})=>{
+      return e
+  })
+  const isWaitingForGetCustomers = useSelector(
+    (state) => state.customer.isWaitingForGetCustomers
+  );
+  const errorInGetCustomers = useSelector(
+    (state) => state.customer.errorInGetCustomers
+  );
+  
+ 
+  
+  return (
+    <Container fluid>
+      <MainPageText text="Customers" />
+      <Container className="container my-5 pb-4 bg-white rounded-2 shadow-sm">
+        {isWaitingForGetCustomers ? (
+          <SkeltonLoader />
+        ) : errorInGetCustomers ? (
+          <ErrorGettingData />
+        ) : (
+            allCustomers.length > 0 &&
+          <>
+          <MainPageText text="customer Table" />
+         <TableView to='customers' columns={Object.keys(formattedCustomers[0])} data={formattedCustomers} />
+         </>
+        )}
+      </Container>
+    </Container>
+  );
+};
+
+export default Customers;
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import {configureStore} from '@reduxjs/toolkit' 
+import buisnessSlice from './buisnessSlice'
+import customerSlice from './customerSlice'
+import ordersSlice from './orderSlice'
+import driverSlice from './driveSlice'
+import pendingBuisnessSlice from './pendingBuisnessSlice'
+import pendingDriversSlice from './pendingDriversSlice'
+import toastSlice from './toastSlice'
+const store = configureStore({
+    reducer : {
+        buisness : buisnessSlice.reducer ,
+        customer : customerSlice.reducer , 
+        order : ordersSlice.reducer  ,
+        driver : driverSlice.reducer , 
+        pendingBuisness : pendingBuisnessSlice.reducer , 
+        pendingDrivers : pendingDriversSlice.reducer , 
+        toast : toastSlice.reducer ,
+    }
+})
+
+export default store
